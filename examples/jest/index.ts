7a51e548f096692027450ad8ec464662c9f78c65
佐藤昭文 <akfm.sato@gmail.com>
2022-08-24 22:37:56 +0900

    impl example of jest

--- examples/jest/index.ts
+++ examples/jest/index.ts
@@ -1,22 +1,4 @@
-{
-  const res = await fetch('http://localhost:9999');
-  console.log(res)
-}
-
-{
-  const res = await window.fetch('http://localhost:9999');
-  console.log(res)
-}
-
-{
-  const res = await globalThis.fetch('http://localhost:9999');
-  console.log(res)
-}
-
-{
-  const dummy = {
-    fetch: () => false,
-  }
-  const res = await dummy.fetch('http://localhost:9999');
-  console.log(res)
+export const localFetcher = async () => {
+  const res = await fetch('http://localhost:9999')
+  return res.json()
 }
